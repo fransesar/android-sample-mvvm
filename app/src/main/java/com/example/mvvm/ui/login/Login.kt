b6658e23a878e0e07a1862a175be4c7daa2ea117
esar <caesar@dragoncapital.center>
2020-06-25 13:49:02 +0700

    4th commit

--- app/src/main/java/com/example/mvvm/ui/login/Login.kt
+++ app/src/main/java/com/example/mvvm/ui/login/Login.kt
@@ -5,11 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mvvm.R
+import com.example.mvvm.databinding.ContentDogsListBinding
+import com.example.mvvm.databinding.ContentLoginBinding
+import com.example.mvvm.ui.dogslist.DogsList
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class Login : Fragment() {
 
+    private lateinit var binding: ContentLoginBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.content_login, container, false)
+        binding = ContentLoginBinding.inflate(inflater, container, false).apply {
+            lifecycleOwner = viewLifecycleOwner
+            btnSkip.setOnClickListener {
+                view?.findNavController()?.navigate(R.id.actionSkip)
+            }
+        }
+        return binding.root
     }
 }
